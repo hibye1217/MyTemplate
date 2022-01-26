5be782c6a0abba3e3f24b4ab47068cd24b0bbd5b
NAF <46581553+hibye1217@users.noreply.github.com>
2022-01-27 02:40:42 +0900

    Add files via upload
    Main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define PRECISION 0
+using namespace std;
+
+
+
+void Main(){
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cout.setf(ios::fixed);
+	cout.precision(PRECISION);
+	Main();
+}
