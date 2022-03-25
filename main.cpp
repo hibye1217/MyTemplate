ff74f0762748a7480a7216d990cbb25f8a95d919
NAF <46581553+hibye1217@users.noreply.github.com>
2022-03-25 22:47:42 +0900

    main.cpp
    2 templates.
    1 for PS, 1 for CP.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
+/// Solving
 #include <bits/stdc++.h>
 #define endl '\n'
 #define PRECISION 0
 using namespace std;
+
+
+
+void Main(){
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cout.setf(ios::fixed);
+	cout.precision(PRECISION);
+	Main();
+}
+
+/// Competition
+#include <bits/stdc++.h>
+#define endl '\n'
+#define PRECISION 0
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define fr first
+#define sc second
+using pi2 = pair<int, int>;
+using pl2 = pair<ll, ll>;
+#define all(v) v.begin(), v.end()
+#define unq(v) sort( all(v) ); v.erase( unique( all(v) ), v.end() );
 
 
 
